26a4a1edf60b093b6be6e3ff7b5c75e242c7b882
IMBABOT1 <zlovred12@gmail.com>
2021-01-28 18:26:55 +0300

    rock

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -7,43 +7,56 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MyGdxGame extends ApplicationAdapter {
-	static class Rock{
+	static class Rock {
 		Texture texture;
 		float x, y;
 		float vx, vy;
 		float r;
+		float scale;
+		float halfSize;
+		float angle;
 
-		public Rock(){
+		public Rock() {
 			texture = new Texture("asteroid64.png");
+			if (texture.getWidth() != texture.getHeight()) throw new RuntimeException("square Error");
+			halfSize = texture.getWidth() / 2;
 			x = 200;
 			y = 600;
 			vx = 500;
 			vy = 0;
-			r = 26;
+			scale = 1.0f;
+			angle = 0.0f;
+			r = 24 * scale;
 		}
 
-		public void render(SpriteBatch batch){
-			batch.draw(texture, x - 32, y - 32);
+		public void render(SpriteBatch batch) {
+			batch.draw(texture, x - halfSize, y - halfSize, halfSize, halfSize, 64, 64, scale, scale,angle, 0, 0, 64, 64, false, false);
 		}
 
-		public void update(float dt){
+		public void update(float dt) {
 			vy -= 500 * dt;
 			x += vx * dt;
 			y += vy * dt;
 			vx *= 0.995f;//сила трения воздуха
 			vy *= 0.995f;//сила трения воздуха
-			if (y - r < GROUND_HEIGHT && vy < 0){
+			if (y - r < GROUND_HEIGHT && vy < 0) {
 				vx *= 0.94f;//сила трения земли
 				y = GROUND_HEIGHT + r;//удоряется о землю
 				vy *= -0.6;// скорость отбрасывания от земли
 			}
-			if (x + r > 1280){
+			if (x + r > 1280) {
 				x = 1280 - r;
 				vx *= -1;
+			}else {
+				angle -= vx * dt;
 			}
-			if (x - r < 0){
+			if (x - r < 0) {
 				x = 0 + r;
 				vx *= -1;
+			}
+			if (Gdx.input.justTouched()) {
+				vx = Gdx.input.getX() - x;
+				vy = (720 - Gdx.input.getY()) - y;
 			}
 		}
 	}
@@ -53,21 +66,20 @@ public class MyGdxGame extends ApplicationAdapter {
 	Rock rock;
 	private static final int GROUND_HEIGHT = 100;
 
-	
+
 	@Override
-	public void create () {
+	public void create() {
 		batch = new SpriteBatch();
 		rock = new Rock();
 		textureGround = new Texture("ground.png");
 	}
 
 
-
 	@Override
-	public void render () {
+	public void render() {
 		float dt = Gdx.graphics.getDeltaTime();
 		update(dt);
-		Gdx.gl.glClearColor(1, 1, 1, 1);
+		Gdx.gl.glClearColor(0.8f, 0.8f, 1.0f, 1.0f);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(textureGround, 0, 0);
@@ -75,12 +87,13 @@ public class MyGdxGame extends ApplicationAdapter {
 		batch.end();
 	}
 
-	public void update(float dt){
+	public void update(float dt) {
 		rock.update(dt);
 	}
-	
+
 	@Override
-	public void dispose () {
+	public void dispose() {
 		batch.dispose();
 	}
 }
+
